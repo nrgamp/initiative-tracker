0fbc81f312f274619cacf9a99c1a76b064614d83
nrgamp <amp.nrgproductions@gmail.com>
2019-10-15 15:16:14 -0400

    removed depricated code

--- src/Tracker-single.js
+++ src/Tracker-single.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './tracker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
